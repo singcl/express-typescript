d207775e8d446f84d49fc78ab72fb3d1ee757124
singcl <24661881@qq.com>
2018-05-09 19:02:57 +0800

    :art: add dependencis and update app.ts

--- app.ts
+++ app.ts
@@ -1,2 +1,8 @@
 ///<reference path='node/node.d.ts' />
 ///<reference path='node/express.d.ts' />
+
+import express = require('express');
+import http = require('http');
+import path = require('path');
+
+let app = express();
