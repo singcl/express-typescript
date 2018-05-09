c99aa371b9c478b10ad92111d60ab40ca646ed6a
singcl <24661881@qq.com>
2018-05-10 01:03:58 +0800

    :art: Add @types/node @types/express instead of XML path.

--- app.ts
+++ app.ts
@@ -1,6 +1,11 @@
-/* tslint:disable no-reference */
-///<reference path='node/node.d.ts' />
-///<reference path='node/express.d.ts' />
+/* tslint:disable no-reference only-arrow-functions*/
+// 手动方法引入node 和 express 的type definitions 文件
+// definitely typed
+/// <reference path='node/node.d.ts' />
+/// <reference path='node/express.d.ts' />
+
+// 或者安装相关@types 依赖以自动引入。 如@types/express @types/node
+// 就不需要上面的三斜线 XML 方式手动引入了。
 
 import express = require('express');
 import http = require('http');
@@ -9,3 +14,6 @@ import path = require('path');
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
+app.listen(3000, function() {
+    console.log('Success at port 3000');
+});
